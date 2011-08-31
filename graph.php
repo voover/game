cf85696640c0acc5bd026e246212883527a8c078
Maciek <mwawro@fb.com>
2011-08-31 16:14:47 -0700

    c

--- graph.php
+++ graph.php
@@ -4,6 +4,6 @@ $ch = curl_init('https://graph.facebook.com/maciek.wawro');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $response = curl_exec($ch);
 curl_close($ch);
-echo "<html><head></head><body>"
+echo "<html><head></head><body>";
 echo $response;
 echo "</body></html>";
